fc0fc9f7fd96f187c339c960d7ebde6fd7e1c107
sirat-hub <siratsmail47@gmail.com>
2026-03-28 11:52:40 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Studysync/',
   plugins: [
     tailwindcss(),
     react()
   ],
   server: {
     host: true,
-    allowedHosts: true,
   }
 })
